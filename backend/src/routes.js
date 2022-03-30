d29b58bea3dd4b3364b39376c98c6c7d5f994133
VinicinBraga <vinicius@bragamatta.com.br>
2022-03-30 15:01:52 -0300

    Structuring Back Crud - Complete

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const AnnotetionController = require("./controllers/AnnotetionController");
 const PriorityController = require("./controllers/PriorityController");
+const ContentController = require("./controllers/ContentController");
 
 routes.post("/annotations", AnnotetionController.create);
 routes.get("/annotations", AnnotetionController.read);
@@ -10,5 +11,7 @@ routes.delete("/annotations/:id", AnnotetionController.delete);
 
 routes.get("/priorities", PriorityController.read);
 routes.post("/priorities/:id", PriorityController.update);
+
+routes.post("/contents/:id", ContentController.update);
 
 module.exports = routes;
