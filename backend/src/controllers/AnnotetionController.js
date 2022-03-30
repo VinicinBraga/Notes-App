faeb92c8c70caa2466d0e354425078cc849794fc
VinicinBraga <vinicius@bragamatta.com.br>
2022-03-30 14:39:03 -0300

    Structuring Back Crud

--- backend/src/controllers/AnnotetionController.js
+++ backend/src/controllers/AnnotetionController.js
@@ -1,15 +1,32 @@
-const { request } = require("express");
-const Annotation = require("../models/AnnotationData");
+const Annotations = require("../models/AnnotationData");
 
 module.exports = {
-  create(req, res) {
-    const { title, notes, priority } = request.body;
-    console.log(title);
-    console.log(notes);
-    console.log(priority);
+  async create(req, res) {
+    const { title, notes, priority } = req.body;
+    if (!notes || !title) {
+      return res.status(400).json({ error: "Title/Annotation Required" });
+    }
+    const annotationCreated = await Annotations.create({
+      title,
+      notes,
+      priority,
+    });
+    return res.json(annotationCreated);
   },
 
-  test(req, res) {
-    res.send("Ta dando certo filhão");
+  async read(req, res) {
+    const annotationsList = await Annotations.find();
+
+    return res.json(annotationsList);
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const annotationsDeleted = await Annotations.findOneAndDelete({ _id: id });
+    if (annotationsDeleted) {
+      return res.json(`${annotationsDeleted._id} has been deleted.`);
+    }
+    return res.status(401).json({ error: "item not found." });
   },
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const routes = express.Router();
 
 const AnnotetionController = require("./controllers/AnnotetionController");
+const PriorityController = require("./controllers/PriorityController");
 
-routes.get("/", AnnotetionController.test);
-routes.get("/annotations", AnnotetionController.create);
+routes.post("/annotations", AnnotetionController.create);
+routes.get("/annotations", AnnotetionController.read);
+routes.delete("/annotations/:id", AnnotetionController.delete);
+
+routes.get("/priorities", PriorityController.read);
+routes.post("/priorities/:id", PriorityController.update);
 
 module.exports = routes;
